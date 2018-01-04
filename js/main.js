826ed4df8b52a263d3166604d34bd9aa759e549d
mwerumuchai <mwerumuchai@gmail.com>
2018-01-04 09:16:55 +0300

    Add photos and fix bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 
+
 function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -22,7 +23,7 @@ $('#nav').affix({
       offset: {
         top: $('header').height()
       }
-});	
+});
 
 	// skills chart
 	$(document).ready(function(e) {
@@ -32,15 +33,15 @@ $('#nav').affix({
 		var top = $('#skills').height()-$(window).scrollTop();
 		console.log(top)
 		if(top<-300){
-			if(index==0){	
-			
+			if(index==0){
+
 				$('.chart').easyPieChart({
 					easing: 'easeOutBounce',
 					onStep: function(from, to, percent) {
 						$(this.el).find('.percent').text(Math.round(percent));
 					}
 				});
-			
+
 				}
 			index++;
 		}
@@ -76,22 +77,22 @@ $('#nav').affix({
         });
 
     });
-	
-	  	
+
+
     // CounterUp
 	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
+		if($("span.count").length > 0){
 			$('span.count').counterUp({
 					delay: 10, // the delay time in ms
 			time: 1500 // the speed time in ms
 			});
 		}
 	});
-	
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
